1b6d6d25fa65f8550cfb3db06cbf10b4654b3a42
Christian Moll <christian.moll@plentymarkets.com>
2018-10-16 09:13:49 +0200

    fixed build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,166 @@
-const gulp = require('gulp');
-const del = require('del');
-const ts = require('gulp-typescript');
+var gulp = require('gulp');
+var del = require('del');
+var sourcemaps = require('gulp-sourcemaps');
+var merge = require('merge2');
 var tsc = require('gulp-typescript');
 var tsProject = tsc.createProject('./tsconfig.json', {typescript: require('typescript')});
+var config = require('./gulp.config.js')();
+var fs = require('fs');
+var semver = require('semver');
+var argv = require('yargs').argv;
+
+var version, increment, preid;
 
 gulp.task('clean-dist', function () {
-    return del('./dist/');
+    return del(config.destinations.tsOutputPath);
 });
 
-gulp.task("compile-ts", function () {
-    return gulp.src('./src/**/!(*.d).ts')
-        .pipe(ts({
-            noImplicitAny: false,
-            outFile: 'output.js'
-        }))
-        .pipe(gulp.dest('built/local'));
-});
-//
-// gulp.task('copy-files', function () {
-//     return gulp.src(['package.json',
-//         'README.md',
-//         './src/**/*'])
-//         .pipe(gulp.dest('./dist/'));
-// });
+//compile typescript files
+gulp.task('compile-ts', function () {
+    var sourceTsFiles = [
+        config.excluded,
+        config.fileSelectors.allTs,
+        config.sources.customLintRules,
+        config.excludedAlso
+    ];
 
-gulp.task('build', gulp.series(
-    'clean-dist',
-    'compile-ts',
-    // 'copy-files'
-));
+    var tsResult = gulp.src(sourceTsFiles)
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
+
+    return merge([
+        tsResult.dts.pipe(gulp.dest(config.destinations.tsOutputPath)),
+        tsResult.js.pipe(sourcemaps.write('.')).pipe(gulp.dest(config.destinations.tsOutputPath))
+    ]);
+});
+
+gulp.task('copy-schematics-files', function () {
+    var sourceTsFiles = [
+        config.excluded,
+        config.fileSelectors.allTs,
+        config.sources.customLintRules
+    ];
+
+    var tsResult = gulp.src(sourceTsFiles)
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
+
+    return merge([
+        tsResult.dts.pipe(gulp.dest(config.destinations.tsOutputPath)),
+        tsResult.js.pipe(sourcemaps.write('.')).pipe(gulp.dest(config.destinations.tsOutputPath))
+    ]);
+});
+
+//copy files to dist
+gulp.task('copy-files', function () {
+    return gulp.src(['package.json',
+        config.fileSelectors.allCSS,
+        config.fileSelectors.allSCSS,
+        config.fileSelectors.allHTML,
+        config.fileSelectors.allSchems,
+        config.fileSelectors.allSchemsExclude,
+        config.fileSelectors.allSchemsExcludeTwo])
+        .pipe(gulp.dest(config.destinations.tsOutputPath));
+});
+
+//copy fonts to dist
+gulp.task('copy-fonts', function () {
+    return gulp.src(config.fileSelectors.allFonts)
+        .pipe(gulp.dest(config.destinations.fontsOutputPath));
+});
+
+//copy images to dist
+gulp.task('copy-images', function () {
+    return gulp.src(config.fileSelectors.allImages)
+        .pipe(gulp.dest(config.destinations.imagesOutputPath));
+});
+
+//copy lang to dist
+gulp.task('copy-lang', function () {
+    return gulp.src(config.fileSelectors.allLang)
+        .pipe(gulp.dest(config.destinations.langOutputPath));
+});
+
+//copy lang to dist
+gulp.task('copy-tslint-rules', function ()
+{
+    return gulp.src(config.sources.tslintRules)
+        .pipe(gulp.dest(config.destinations.tsOutputPath));
+});
+
+/**
+ *
+ * usage: 'npm run build' for local build
+ *
+ **/
+gulp.task('build',
+    gulp.series(
+        'clean-dist',
+        'compile-ts',
+        'copy-files'
+    )
+);
+
+/**
+ * define gulp tasks for 'npm-publish'
+ */
+//changing version of package.json for new publish
+function changeVersion(done) {
+    var json = JSON.parse(fs.readFileSync('./package.json'));
+
+    console.log('-------------------------------------------------');
+    console.log('--- OLD PACKAGE VERSION: ' + json.version + ' ---');
+
+    json.version = semver.inc(json.version, increment, preid);
+
+    version = json.version;
+
+    console.log('--- NEW PACKAGE VERSION: ' + json.version + ' ---');
+    console.log('-------------------------------------------------');
+
+    fs.writeFileSync('./package.json', JSON.stringify(json, null, '\t'));
+    done();
+};
+
+/**
+ *
+ * usage: 'npm run publish -- --param1 --param2 param2_value' for publishing
+ *
+ * @param increment  - Possible values are
+ *                      major           (1.x.x to 2.x.x),
+ *                      premajor        (1.x.x to 2.x.x-0 or 2.x.x-subversion.0),
+ *                      minor           (x.1.x to x.2.x),
+ *                      preminor        (x.1.x to x.2.x-0 or x.2.x-subversion.0)
+ *                      patch           (x.x.1 to x.x.2),
+ *                      prepatch        (x.x.1 to x.x.2-0 or x.x.1 to x.x.2-subversion.0)
+ *                      or prerelease   (x.x.x-0 or x.x.x-subversion.0 to x.x.x-1 or x.x.x-subversion.1)
+ *                     If not set patch is default. See VERSIONING.md for further information.
+ * @param preid      - Sets a subversion (appends '-param_value', e.g. x.x.x-newFeature, to version in package.json) for a premajor,
+ *     preminor or prepatch release. Use only, if really necessary!!
+ * @param target     - Actually not implemented!! Sets the target directory to copy build files to. Will copy files to
+ *     'node_modules/@plentymarkets/terra-components' in target directory
+ *
+ **/
+gulp.task('npm-publish', function () {
+    increment = argv.increment ? argv.increment : 'patch';
+    preid = argv.preid ? argv.preid : '';
+
+    if(argv.level || argv.subversion)
+    {
+        console.log('-------------------------------------------------------------------');
+        console.log('----  Build not started. See gulpfile for further information. ----');
+        console.log('-------------------------------------------------------------------');
+
+        return;
+    }
+    else
+    {
+        return gulp.series(
+            changeVersion,
+            'clean-dist',
+            'compile-ts',
+            'copy-files'
+        );
+    }
+}());
+
